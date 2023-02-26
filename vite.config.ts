d5bce4c94e47233834274b07da8c37c31a66e103
Peter Jaško <peter.jasko@vacuumlabs.com>
2023-02-26 15:49:55 +0100

    Manually code-split

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('apexcharts')) return 'charts'
+          if (id.includes('node_modules')) return 'vendor'
+        },
+      },
+    },
+  }
 })
